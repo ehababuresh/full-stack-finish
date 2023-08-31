a0844ff77270835607fe0f11df5fba5db071925b
Ehab Aburesh <110368166+ehababuresh@users.noreply.github.com>
2023-08-31 11:20:48 +0300

    add

--- client/src/layout/header/TopNavBar/Logo/Logo.jsx
+++ client/src/layout/header/TopNavBar/Logo/Logo.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import NavBarLink from "../../../../routes/components/NavBarLink";
 import ROUTES from "./../../../../routes/routesModel";
 
 const Logo = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <NavBarLink to={ROUTES.CARDS}>
       <Typography
@@ -12,7 +22,12 @@ const Logo = () => {
           display: { xs: "none", md: "inline-flex" },
           marginRight: 2,
           fontFamily: "fantasy",
-        }}>
+          fontSize: isHovered ? "2.2rem" : "2rem",
+          transition: "font-size 0.2s ease-in-out",
+        }}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         eab
       </Typography>
     </NavBarLink>
