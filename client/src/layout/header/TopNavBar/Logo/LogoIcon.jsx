6f3df3e2cb9e53235d24bedb05c15812aa65775b
Ehab Aburesh <110368166+ehababuresh@users.noreply.github.com>
2023-09-03 01:48:05 +0300

    add

--- client/src/layout/header/TopNavBar/Logo/LogoIcon.jsx
+++ client/src/layout/header/TopNavBar/Logo/LogoIcon.jsx
@@ -1,25 +1,49 @@
-import React from "react";
-import IconButton from "@mui/material/IconButton";
-import Avatar from "@mui/material/Avatar";
-import NavBarLink from "../../../../routes/components/NavBarLink";
-import ROUTES from "./../../../../routes/routesModel";
+import React, { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
 const LogoIcon = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    
-    <NavBarLink to={ROUTES.ROOT}>
+    <div>
       <IconButton
-        sx={{ display: { xs: "inline-flex", md: "none" } }}
-        size="large"
-        edge="start"
-        color="inherit"
-        aria-label="menu">
-        <Avatar
-          alt="Business card icon"
-          src="/assets/images/business-card.png"
-        />
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+        sx={{ display: { xs: 'inline-flex', md: 'none' } }}
+      >
+        <Avatar alt="Business card icon" src="/assets/images/business-card.png" />
       </IconButton>
-    </NavBarLink>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+          <MenuItem onClick={handleClose}>
+          <Link to="/cards">cards</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/about">About</Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link to="/fav-cards">Favorite Cards</Link>
+        </MenuItem>
+      </Menu>
+    </div>
   );
 };
 
